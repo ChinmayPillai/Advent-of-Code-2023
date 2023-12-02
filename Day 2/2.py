5fc5b0eb3249d4e5e39b40ad64e85630535c5ad7
Chinmay Pillai <chinmay.pillai@gmail.com>
2023-12-02 11:29:48 +0530

    Day 2

--- Day 2/2.py
+++ Day 2/2.py
@@ -1,14 +1,51 @@
-file_path = "2.txt"
-test_path = "test.txt"
+day = 2
+file_path = "Day " + str(day) + "/" + str(day) + ".txt"
+test_path = "Day " + str(day) + "/test.txt"
 ans = 0
 
+sum = 0
 
-with open(test_path, 'r') as file:
+max = {
+    "red": 0,
+    "green": 0,
+    "blue": 0
+}
+
+with open(file_path, 'r') as file:
     lines = file.readlines()
 
     for line in lines:
-        for ch in line:
-            ans = 0
+        max["red"] = 0
+        max["green"] = 0
+        max["blue"] = 0
 
 
-print(ans)
+        game_info, rolls_info = line.split(':')
+        game_id = int(game_info.split()[1])
+        #sum += game_id
+        
+        rolls = rolls_info.strip().split(';')
+
+
+        for roll in rolls:
+
+            flag = False
+            for item in roll.split(','):
+                count, colour = item.strip().split()
+                if int(count) > max[colour]:
+                    max[colour] = int(count)
+                    #flag = True
+                    #ans += game_id
+                    #break
+            
+            #if flag:
+            #    break
+        
+        sum += max["red"] * max["green"] * max["blue"]
+
+
+
+
+print(sum)
+
+#print(sum-ans)
--- template.py
+++ template.py
@@ -1,7 +1,9 @@
-file_path = "2.txt"
-test_path = "test.txt"
-ans = 0
+day = 2
+file_path = "Day " + str(day) + "/" + str(day) + ".txt"
+test_path = "Day " + str(day) + "/test.txt"
 
+
+ans = 0
 
 with open(file_path, 'r') as file:
     lines = file.readlines()
