ab10ef89e9c5752592d0ca3044232d5c125202ba
Chinmay Pillai <chinmay.pillai@gmail.com>
2023-12-20 10:24:53 +0530

    Update Template

--- template.py
+++ template.py
@@ -1,17 +1,22 @@
-day = 11
+day = 20
 file_path = "Day " + str(day) + "/" + str(day) + ".txt"
 test_path = "Day " + str(day) + "/test.txt"
+
+def valid(grid, i, j):
+    if 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+        return True
+    return False
+
+
 
 
 for path in [test_path, file_path]:
     
     ans = 0
-
     with open(path, 'r') as file:
         lines = [line.strip() for line in file.read().split('\n')]
 
-    for i, line in enumerate(lines):
-        pass
+    
 
     
     print("="*80)
